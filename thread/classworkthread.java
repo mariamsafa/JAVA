48cda3655e000cface337ca9b95e514c9a8e3ce0
mariamsafa <mariamsafa153@gmail.com>
2024-05-05 02:31:45 +0600

    sync

--- thread/classworkthread.java
+++ thread/classworkthread.java
@@ -67,7 +67,7 @@ here 5 is the default priority and my thread is the thread's name and "main" is 
 
 
  //Creating Multiple Threads
-class MultithreadingDemo implements Runnable {
+/* class MultithreadingDemo implements Runnable {
     public void run()
     {
         try {
@@ -94,4 +94,60 @@ public class classworkthread {
     }
     
 }
+} */
+
+
+
+
+
+//Synchronized
+class Callme {
+    void call(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
 }
+
+class Caller implements Runnable {
+    String msg;
+    Callme target;
+    Thread t;
+
+    public Caller(Callme targ, String s) {
+        target = targ;
+        msg = s;
+        t = new Thread(this);
+        t.start();
+    }
+
+    public void run() {
+        synchronized (target) {
+            target.call(msg);
+        }
+    }
+}
+
+public class classworkthread {
+   
+       public static void main(String[] args) {
+           
+    Callme target = new Callme();
+    Caller ob1 = new Caller(target, "Hello");
+    Caller ob2 = new Caller(target, "Synchronized");
+    Caller ob3 = new Caller(target, "World");
+    // wait for threads to end
+    try {
+    ob1.t.join();
+    ob2.t.join();
+    ob3.t.join();
+    } catch(InterruptedException e) {
+    System.out.println("Interrupted");
+    }
+    }
+   
+   }
